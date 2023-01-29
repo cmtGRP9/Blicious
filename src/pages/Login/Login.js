0848a348d50a0549844eef71bf1818a7f08f96ec
cmtGRP9 <123905413+cmtGRP9@users.noreply.github.com>
2023-01-30 05:58:33 +0800

    Add files via upload

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Signup, SignIn, ForgotPassword } from 'pages';
+import {
+	Switch,
+	Route,
+	Redirect,
+	useLocation,
+} from 'react-router-dom';
+
+import { LoginContainer, LoginCard } from './LoginElements.js';
+
+import { useAuth } from 'contexts';
+
+export const Login = () => {
+	const { currentUser } = useAuth();
+	const { query } = useLocation();
+	if (currentUser)
+		return <Redirect to={{ pathname: '/user', query: query }} />;
+	return (
+		<LoginContainer>
+			<LoginCard>
+				<Switch>
+					<Route path="/login" component={SignIn} />
+					<Route path="/signup" component={Signup} />
+				</Switch>
+			</LoginCard>
+		</LoginContainer>
+	);
+};
